908fba2c2b4cc63981277cf3673e47b887908e59
David Rolfs <davidrolfs15@gmail.com>
2017-09-15 16:55:33 -0700

    add camer follow to player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Vector2 velocity;
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject player;
+
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+		
+	}
+
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+
+}
